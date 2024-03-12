5b468e190863ecadc83010b87224ca394bdff197
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-03-12 12:53:12 +0530

    change

--- calculator/calculator/src/App.js
+++ calculator/calculator/src/App.js
@@ -4,13 +4,20 @@ import Wrapper from "./components/wrapper";
 import Screen from "./components/screen";
 import ButtonBox from "./components/buttobBox.js";
 import Button from "./components/button";
-
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   return (
     <Wrapper>
       <Screen value="0" />
       <ButtonBox>
+        
         <Button
           className=""
           value="0"
