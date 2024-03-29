70e5e59ddfa007208f36840b80ee8d5f05da122a
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-03-29 15:48:34 +0530

    change

--- new/src/Components/wrapper.js
+++ new/src/Components/wrapper.js
@@ -5,25 +5,30 @@ import { useState } from 'react';
 
 function Wrapper(){
     const [squares, setSquares] = useState(Array(9).fill('‎'));
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        setSquares(nextSquares);
+      }
 
     return(
         <div className="wrapper">
             <div className="board-row">
-                <Box value={squares[0]}></Box>
-                <Box value={squares[1]}></Box>
-                <Box value={squares[2]}></Box>
+                <Box value={squares[0]} onSquareClick={handleClick(0)}></Box>
+                <Box value={squares[1]} onSquareClick={handleClick(1)}></Box>
+                <Box value={squares[2]} onSquareClick={handleClick(2)}></Box>
 
             </div>
             <div className="board-row">
-                <Box value={squares[3]}></Box>
-                <Box value={squares[4]}></Box>
-                <Box value={squares[5]}></Box>
+                <Box value={squares[3]} onSquareClick={handleClick(3)}></Box>
+                <Box value={squares[4]} onSquareClick={handleClick(4)}></Box>
+                <Box value={squares[5]} onSquareClick={handleClick(5)}></Box>
 
             </div> 
             <div className="board-row">
-                <Box value={squares[6]}></Box>
-                <Box value={squares[7]}></Box>
-                <Box value={squares[8]}></Box>
+                <Box value={squares[6]} onSquareClick={handleClick(6)}></Box>
+                <Box value={squares[7]} onSquareClick={handleClick(7)}></Box>
+                <Box value={squares[8]} onSquareClick={handleClick(8)}></Box>
 
             </div>
         </div>
