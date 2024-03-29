dd1836e84a142680d6096728c7314f696aa8abc7
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-03-29 16:54:37 +0530

    change

--- new/src/Components/wrapper.js
+++ new/src/Components/wrapper.js
@@ -8,7 +8,7 @@ function Wrapper(){
     const [squares, setSquares] = useState(Array(9).fill('‎'));//initially all the boxes are set to null and we are doin this in the parent element rather than the child element
     //the parent element is used to record so that we can keep track odf the states of all the boxes without using them
     function handleClick(i) {
-        if (squares[i]!=='‎') {//used to check if the square is already used or not
+        if (squares[i]!=='‎' || calculateWinner(squares)) {//used to check if the square is already used or not
           return;
         }
         const nextSquares = squares.slice();//the entire thing is sliced into an array
@@ -19,7 +19,16 @@ function Wrapper(){
         }
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
-      }//next onSquareClick is a prop function that is passed after clicking on the button
+      }
+      const winner = calculateWinner(squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+      }
+      
+      //next onSquareClick is a prop function that is passed after clicking on the button
       //now the value of the squareClick is decided by handleclick(i)
     return(
         <div className="wrapper">
@@ -48,3 +57,23 @@ function Wrapper(){
 
 export default Wrapper;
 //basically the main box 
+//now we write a code to declare the winner
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];//all the possibilities of winning the game
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
