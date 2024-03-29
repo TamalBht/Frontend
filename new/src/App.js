650bc8b4301eba66e9d003ce9b602c0a55707c12
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-03-29 15:51:49 +0530

    changes

--- new/src/App.js
+++ new/src/App.js
@@ -1,13 +1,10 @@
 import Wrapper from './Components/wrapper.js';
-import Box from"./Components/box.js";
+
 import './App.css';
 
 function App() {
   return (
     <Wrapper/>
-      
-    
-     
   );
 }
 
