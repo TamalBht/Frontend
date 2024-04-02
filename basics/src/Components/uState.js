5aad4bb06fb6e92af4f8b2b98d9fe5ef5f231718
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-03 01:40:55 +0530

    change

--- basics/src/Components/uState.js
+++ basics/src/Components/uState.js
@@ -1,13 +1,24 @@
 import {useState} from 'react';
+import './button.css'
+
 function Theme(){
-    const [initial,update]= useState(true);
+    const [initial,update]= useState();
+    const color1="black";
+    const color2="beige";
     function change(){
        if(initial){
-            
+          document.body.style.background=color1; 
+          update(false);
+ 
+       }
+       else{
+        document.body.style.background=color2;  
+        update(true);
+
        } 
     }
     return(
-        <button>Click me</button>
+        <button onClick={change} className='changee'>Click me</button>
     );
 }
 export default Theme;
