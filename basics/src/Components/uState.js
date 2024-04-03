cc4231231c0bb17089f81dfa20415dc1057e845c
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-04 00:44:32 +0530

    change

--- basics/src/Components/uState.js
+++ basics/src/Components/uState.js
@@ -5,12 +5,19 @@ function Theme(){
     const [initial,update]= useState();
     const color1="black";
     const color2="beige";
+    document.body.style.background=color2;
+    document.body.style.color=color1;
+
+
     function change(){
        if(initial){
           document.body.style.background=color1;
           document.body.style.color=color2;
           update(false);
- 
+          document.body.style.background=color2;
+          document.body.style.color=color1;
+
+
        }
        else{
         document.body.style.background=color2;
