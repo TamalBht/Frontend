8529375bdcb5f1fb969f21ef35ee566443e98e6d
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-12 23:31:42 +0530

    date and time update

--- weather/src/Components/dateTime.js
+++ weather/src/Components/dateTime.js
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import "./dateTime.css"
-const DateTime=()=>{
-    
-    const dayOfWeek = new Date().toLocaleDateString(undefined, { weekday: 'long' });
 
-    return(
-        <div className='dateTime'>
-            <div className="day">Wednesday</div>
-        </div>
-    );
+const DateTime = () => {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  const dayOfWeek = dateTime.toLocaleDateString(undefined, { weekday: 'long' });
+  const time = dateTime.toLocaleTimeString();
+
+  return (
+    <div className='dateTime'>
+      <div className="day">{dayOfWeek}</div>
+      <div className="time">{time}</div>
+    </div>
+  );
 }
-export default  DateTime;
+
+export default DateTime;
