f1df59bba6d45253de17678cb506e4814a080e9f
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-12 22:37:01 +0530

    change

--- weather/src/Components/dateTime.js
+++ weather/src/Components/dateTime.js
@@ -1,11 +1,10 @@
 import "./dateTime.css"
 const DateTime=()=>{
-    const dateString = new Date().toLocaleString();
+    
     const dayOfWeek = new Date().toLocaleDateString(undefined, { weekday: 'long' });
 
     return(
         <div className='dateTime'>
-            <div className="date">{dateString}</div>
             <div className="day">{dayOfWeek}</div>
         </div>
     );
