184d31df2859e2f825221117c69e537d00a4916c
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-22 20:21:08 +0530

    changes

--- login-signup/src/Components/Login.js
+++ login-signup/src/Components/Login.js
@@ -2,7 +2,10 @@ import "./Login.css"
 function Login(){
     return(
         <form className="login">
-            
+            <label for="nname">Name:</label>
+            <input type="text" id="nname" name="nname" placeholder="Enter your name"/><br></br>
+            <label for="mail">E-mail:</label>
+            <input type="text" id="mail" name="mail" placeholder="Email Address"/><br></br>
         </form>
     );
 }
