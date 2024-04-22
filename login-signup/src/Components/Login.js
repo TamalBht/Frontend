6b0d18e07915f270e4c47dc5df51334635c3a047
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-22 20:32:44 +0530

    changes

--- login-signup/src/Components/Login.js
+++ login-signup/src/Components/Login.js
@@ -1,12 +1,19 @@
 import "./Login.css"
 function Login(){
     return(
+       <div className="login-wrapper">
+        <h1>Login</h1>
         <form className="login">
             <label for="nname">Name:</label>
             <input type="text" id="nname" name="nname" placeholder="Enter your name"/><br></br>
             <label for="mail">E-mail:</label>
             <input type="text" id="mail" name="mail" placeholder="Email Address"/><br></br>
+            <label for="pwd">Password:</label>
+            <input type="password" id="pwd" name="pwd" /><br></br>
         </form>
+        <button>Login</button>
+        
+       </div>
     );
 }
 export default Login;
