b3ac6f6d54f2124f6a78fd453443a520730ddb16
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-04-22 21:38:12 +0530

    changes

--- login-signup/src/Components/Login.js
+++ login-signup/src/Components/Login.js
@@ -7,12 +7,15 @@ function Login(){
        <div className="login-wrapper">
         <h1>Login</h1>
         <form className="login">
-            <label for="nname">Name:</label>
-            <input type="text" id="nname" name="nname" placeholder="Enter your name"/><br></br>
-            <label for="mail">E-mail:</label>
-            <input type="text" id="mail" name="mail" placeholder="Email Address"/><br></br>
-            <label for="pwd">Password:</label>
-            <input type="password" id="pwd" name="pwd" /><br></br>
+            
+           <div className='login-content'>
+           <label for="mail">E-mail:</label>
+            <input type="text"  name="mail" placeholder="Email Address"/><br></br>
+           </div>
+           <div className="login-content">
+           <label for="pwd">Password:</label>
+            <input type="password"  name="pwd" /><br></br>
+           </div>
         </form>
         <button>Login</button>
         <Link to={"/Signup"}>New here?</Link>
--- login-signup/src/Components/Signup.js
+++ login-signup/src/Components/Signup.js
@@ -1,6 +1,26 @@
+import "./Signup.css"
 function Sign(){
     return(
-        <h1>Signup page</h1>
+        <div className="signup-wrapper">
+        <h1>Signup</h1>
+        <form className="signup">
+        <div className='sign-content'>
+           <label for="name">Name:</label>
+            <input type="text"  name="Name" placeholder="Name"/><br></br>
+           </div> 
+           <div className='sign-content'>
+           <label for="mail">E-mail:</label>
+            <input type="text"  name="mail" placeholder="Email Address"/><br></br>
+           </div>
+           <div className="signcontent-content">
+           <label for="pwd">Password:</label>
+            <input type="password"  name="pwd" /><br></br>
+           </div>
+        </form>
+        <button>Signup</button>
+        
+       
+       </div>
     );
 }
 export default Sign;
